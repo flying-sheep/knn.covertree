b59a23289aa875f528662f489ae44e4efb1c33b9
Philipp A <flying-sheep@web.de>
2019-09-18 14:56:06 +0200

    Use expect_equal because I can’t be bothered
    … to debug errors that *just* appear on CRAN

--- tests/testthat/test-knn.r
+++ tests/testthat/test-knn.r
@@ -10,7 +10,7 @@ test_that('knn works the same as FNN', {
 	r_fnn <- FNN::get.knn(e, 5L)
 	
 	dimnames(r_destiny$index) <- dimnames(r_destiny$dist) <- NULL
-	expect_identical(r_destiny$dist, r_fnn$nn.dist)
+	expect_equal(r_destiny$dist, r_fnn$nn.dist)
 })
 
 
@@ -20,9 +20,9 @@ test_that('knnx works the same as to FNN', {
 	e <- as.matrix(datasets::mtcars)
 	am <- factor(e[, 'am'], labels = c('automatic', 'manual'))
 	
-	r_destiny <- find_knn(e[am == 'manual',], 5L, query = e[am == 'automatic',])
+	r_destiny <- find_knn(e[am == 'manual', ], 5L, query = e[am == 'automatic', ])
 	r_fnn <- FNN::get.knnx(e[am == 'manual', ], e[am == 'automatic', ], 5L)
 	
 	dimnames(r_destiny$index) <- dimnames(r_destiny$dist) <- NULL
-	expect_identical(r_destiny$dist, r_fnn$nn.dist)
+	expect_equal(r_destiny$dist, r_fnn$nn.dist)
 })
